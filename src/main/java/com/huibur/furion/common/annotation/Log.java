d5e38fd2b6406aa55b1dcda66d4716b98f5065eb
wenzhijie <wenzhijie@huibur.com>
2020-03-04 11:28:20 +0800

    upd:build configure

--- src/main/java/com/huibur/furion/common/annotation/Log.java
+++ src/main/java/com/huibur/furion/common/annotation/Log.java
@@ -5,9 +5,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * @author MrBird
- */
+/*******************************************************
+ * Copyright(c)2019-2020 HuiBur. All rights reserved.
+ * Header: Log.java
+ * Discussion: log annotation
+ * Create Date: 2019/6/6
+ * Author: Jerry Wen
+ * Version: 1.0
+ *******************************************************/
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Log {
